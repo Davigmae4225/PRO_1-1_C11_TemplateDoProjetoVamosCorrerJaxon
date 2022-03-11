91748a8bd5255867b9c9e81000c97642649aef7f
Davigmae4225 <100358017+Davigmae4225@users.noreply.github.com>
2022-03-11 09:39:11 -0300

    1 commit

--- sketch.js
+++ sketch.js
@@ -1,53 +1,73 @@
-var path,boy, leftBoundary,rightBoundary;
-var pathImg,boyImg;
+var path, boy, leftBoundary, rightBoundary;
+var pathImg, boyImg;
 var i;
+var boy_runnigImag;
 
+function preload() {
+    pathImg = loadImage("path.png");
+    boyImg = loadAnimation("Runner-1.png", "Runner-2.png");
 
-function preload(){
-pathImg = loadImage("path.png");
-boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
-function setup(){
- createCanvas(400,400);
+function setup() {
+    createCanvas(400, 400);
 
-// Movendo o fundo 
-
-
-
-
-//Criando menino que corre 
-
-
-
-// Criando Boundary (Limite) esquerdo  
-leftBoundary=createSprite(0,0,100,800);
-leftBoundary.visible = false;
+    // Movendo o fundo 
+    path = createSprite(200, 200)
+    path.addImage("path", pathImg)
+    path.velocityY = 4;
+    path.scale = 1.2;
+    console.log(path.y)
 
 
-//Crie Boundary direito 
+
+    //Criando menino que corre 
+     boy = createSprite(180,340,30,30)
+     boy.scale = 0.08
+     boy.addAnimation ("running",boyImg)
+
+
+
+    // Criando Boundary (Limite) esquerdo  
+    leftBoundary = createSprite(0, 0, 100, 800);
+    leftBoundary.visible = false;
+
+
+    //Crie Boundary direito 
+    rightBoundary = createSprite(410, 0, 100, 800);
+    rightBoundary.visible = false;
 
 }
 
 
 function draw() {
-background(0);
-path.velocityY = 4;
+    background(0);
+    path.velocityY = 4;
 
-// Menino se movendo no eixo X com o mouse
-edges= createEdgeSprites();
-boy.collide(edges[3]);
+    // Menino se movendo no eixo X com o mouse
 
-
-
- //Reiniciar o fundo
- 
+    boy.x = World.mouseX;
 
 
 
- drawSprites();
+    edges = createEdgeSprites();
+    boy.collide(edges[3]);
+    boy.collide(leftBoundary);
+    boy.collide(rightBoundary);
 
- }
+
+
+    //Reiniciar o fundo
+    if (path.y>400) {
+     path.y = height/2   
+     
+
+    }
+
+
+    drawSprites();
+
+}
 
 
 
